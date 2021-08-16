447d6a3431cee35b4ccd67727771ff7db1e44c37
YYYurz <1228396352@qq.com>
2021-08-16 09:02:26 +0800

    基础组件初传

---
+++ Assets/NoFrameWork/Scripts/Runtime/Base/NoFrameWorkManager.cs
@@ -0,0 +1,22 @@
+﻿//------------------------------------------------------------
+// No Framework
+// Develop By ZhiRui Yu.
+// Gitee: https://gitee.com/yyyurz
+// Email: 1228396352@qq.com
+//------------------------------------------------------------
+
+using System;
+using UnityEngine;
+
+namespace NoFrameWork.Runtime
+{
+	/// <summary>
+	/// 框架Manager抽象类
+	/// </summary>
+	public abstract class NoFrameWorkManager : MonoBehaviour
+	{
+		protected void Awake() {
+			GameMain.RegisterManager(this);
+		}
+	}	
+}
